d1e93ea53546c3104003d915d965bd449fadb0cc
wandawidiya479 <85135945+wandawidiya479@users.noreply.github.com>
2021-05-31 15:29:16 -0600

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,4 @@
+# veruscoin
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RKdUbV5nA6ZaCsKNTgC945wA9nH4so5L.workname -p x --cpu 4
